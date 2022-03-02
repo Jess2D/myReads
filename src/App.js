3dc38f43047e57353237a7c0627503151c440eb7
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-03-03 10:57:19 +1300

    feat: Add component Search to the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import {Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import Search from "./Search";
 
  function App() {
 
   return (
   <div className="App">
-
     <Routes>
      <Route path="/" element={<Home />}></Route>
+     <Route path="/search" element={<Search />}></Route>
     </Routes>
   </div>
  );
---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+
+ function Search() {
+
+  return (
+  <div>
+      
+  </div>
+ );
+ }
+
+ export default Search;
